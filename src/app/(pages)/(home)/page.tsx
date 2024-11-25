c1b02c7711cb32b8f12db71f9bf5964ac62610e4
Lester Cerioli <lesterlucasit@hotmail.com>
2024-11-25 15:31:16 -0800

    issue(#109): Moving components

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import * as S from "./styles";
 import Cookies from "@/components/home/cookies/cookies/cookies";
 import { Banner } from "@/components/home/cookies/homeBanner/homeBanner";
-import { Img } from "@/components/developmentWeb/styles";
+
 
 const Home: React.FC = () => {
   return (
